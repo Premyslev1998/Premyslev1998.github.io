f6433fadc3817fe79f230d37a741efc51c197cf0
Ilya <vodogrejj87@mail.ru>
2019-08-12 15:25:53 +0500

    _optimized_webpack_sass_pug
    Custom_webpack_build

---
+++ _optimized_webpack_sass_pug/src/index.js
@@ -0,0 +1,7 @@
+import './js/common'
+import './css/main.css'
+import './sass/main.sass'
+import React from 'react'
+
+//import 'react' //1th way connection ReactJS
+//window.React = require('react') //another way
